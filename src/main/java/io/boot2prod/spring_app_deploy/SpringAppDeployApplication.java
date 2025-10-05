53e74f832ceb099c426474ef59fe67e9f1b3e620
Daniel Willian <d.willian_sc@hotmail.com>
2025-10-05 13:50:33 -0400

    Add spotless

--- src/main/java/io/boot2prod/spring_app_deploy/SpringAppDeployApplication.java
+++ src/main/java/io/boot2prod/spring_app_deploy/SpringAppDeployApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAppDeployApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAppDeployApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppDeployApplication.class, args);
+    }
 }
--- src/test/java/io/boot2prod/spring_app_deploy/SpringAppDeployApplicationTests.java
+++ src/test/java/io/boot2prod/spring_app_deploy/SpringAppDeployApplicationTests.java
@@ -1,5 +1,8 @@
 package io.boot2prod.spring_app_deploy;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,34 +11,32 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringAppDeployApplicationTests {
-	@LocalServerPort
-	private int port;
+    @LocalServerPort
+    private int port;
 
-	@Autowired
-	private RestClient.Builder restClientBuilder;
+    @Autowired
+    private RestClient.Builder restClientBuilder;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {}
 
-	@SneakyThrows
-	@Test
-	void healthEndpointIsExposed() {
-		RestClient restClient = restClientBuilder
-				.baseUrl("http://localhost:" + port)
-				.build();
-		ResponseEntity<String> resp = restClient.get().uri("http://localhost:" + port + "/actuator/health").retrieve().toEntity(String.class);
-		Assertions.assertThat(resp.getStatusCode().is2xxSuccessful()).isTrue();
-		JsonNode node = objectMapper.readTree(resp.getBody());
-		Assertions.assertThat(node.get("status").asText()).isEqualTo("UP");
-	}
+    @SneakyThrows
+    @Test
+    void healthEndpointIsExposed() {
+        RestClient restClient =
+                restClientBuilder.baseUrl("http://localhost:" + port).build();
+        ResponseEntity<String> resp = restClient
+                .get()
+                .uri("http://localhost:" + port + "/actuator/health")
+                .retrieve()
+                .toEntity(String.class);
+        Assertions.assertThat(resp.getStatusCode().is2xxSuccessful()).isTrue();
+        JsonNode node = objectMapper.readTree(resp.getBody());
+        Assertions.assertThat(node.get("status").asText()).isEqualTo("UP");
+    }
 }
