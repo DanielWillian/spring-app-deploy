72cacecc67fb00e8a1d20aaabd59b5fbc6265cd5
Daniel Willian <d.willian_sc@hotmail.com>
2025-10-05 13:49:41 -0400

    Add test for health check

--- src/test/java/io/boot2prod/spring_app_deploy/SpringAppDeployApplicationTests.java
+++ src/test/java/io/boot2prod/spring_app_deploy/SpringAppDeployApplicationTests.java
@@ -1,13 +1,41 @@
 package io.boot2prod.spring_app_deploy;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
 
-@SpringBootTest
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringAppDeployApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private RestClient.Builder restClientBuilder;
+
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@SneakyThrows
+	@Test
+	void healthEndpointIsExposed() {
+		RestClient restClient = restClientBuilder
+				.baseUrl("http://localhost:" + port)
+				.build();
+		ResponseEntity<String> resp = restClient.get().uri("http://localhost:" + port + "/actuator/health").retrieve().toEntity(String.class);
+		Assertions.assertThat(resp.getStatusCode().is2xxSuccessful()).isTrue();
+		JsonNode node = objectMapper.readTree(resp.getBody());
+		Assertions.assertThat(node.get("status").asText()).isEqualTo("UP");
+	}
 }
