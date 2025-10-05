651b34c3dc4451d265c114b3d16353ad801785c1
Daniel Willian <d.willian_sc@hotmail.com>
2025-10-05 13:02:27 -0400

    Initial commit

---
+++ src/main/java/io/boot2prod/spring_app_deploy/SpringAppDeployApplication.java
@@ -0,0 +1,13 @@
+package io.boot2prod.spring_app_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/boot2prod/spring_app_deploy/SpringAppDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package io.boot2prod.spring_app_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
